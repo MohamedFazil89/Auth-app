6e3abc5dbdacbc631f1a8658145e193cc92a3b71
MohamedFazil89 <nmohammedfazil790@gmail.com>
2024-06-25 23:23:08 +0530

    animation add

--- Pages/HomePage.jsx
+++ Pages/HomePage.jsx
@@ -1,19 +1,41 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Animated  } from "react-native";
 
 function HomePage({ navigation }){
 
+    const InitPosition = new Animated.ValueXY({ x: 0, y: -900})
+
+useEffect(()=>{
+    Animated.sequence([
+        Animated.delay(1000),
+        Animated.spring(InitPosition,{
+            toValue: { x: 0, y: 0 },
+            stiffness: 4,
+            damping: 6,
+            mass: 1,
+            useNativeDriver: true
+        }),
+        
+    ]).start()
+})
+
+
     return(
-        <View style={styles.text}>
+        <Animated.View style={[styles.text, {
+            transform: [
+                { translateX: InitPosition.x},
+                { translateY: InitPosition.y}
+            ]
+        }]}>
 
 
 
-            {/* <Text style={styles.text}> Home Page</Text>
+            <Text style={styles.text}> Home Page</Text>
             <TouchableOpacity onPress={() => navigation.goBack()}>
                 <Text >Go Back</Text>
-            </TouchableOpacity>  */}
+            </TouchableOpacity> 
 
-        </View>
+        </Animated.View>
     )
 }
 
